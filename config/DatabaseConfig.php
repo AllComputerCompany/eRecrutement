d97b1b401bb2de44e66a3bb94677298c74836d30
Bachir Zerourou <Bachir bachir.zerourou@yahoo.fr>
2018-12-18 20:54:40 +0100

    index modification

--- config/DatabaseConfig.php
+++ config/DatabaseConfig.php
@@ -9,23 +9,24 @@
 $app = new \Slim\App([
 	'settings' => [
 		'displayErrorDetails' => true,
-	//Database definition
+	//Database definition for Laraval
 	'db' => [
 		'driver' => 'mysql',
 		'host'=> 'localhost',
-		'database' => 'raw-php',
+		'database' => 'erecrutement',
 		'username' => 'root',
-		'password' => 'basket',
+		'password' => 'root',
 		'charset' => 'utf8',
 		'collation' => 'utf8_unicode_ci',
 		'prefix' => '',
 	],
+	// for cakePHP
 	'cakeDB' => [
 		'className' => 'Cake\Database\Connection',
 		'driver' => 'Cake\Database\Driver\Mysql',
-		'database' => 'raw-php',
+		'database' => 'erecrutement',
 		'username' => 'root',
-		'password' => 'basket',
+		'password' => 'root',
 		'cacheMetadata' => false // If set to `true` you need to install the optional "cakephp/cache" package.
 
 	]
